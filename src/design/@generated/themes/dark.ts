6faa1f8d730891eb4099ed2e8c1387463b80d677
Tony Vi <winogradovaa@gmail.com>
2022-05-27 21:02:59 +0300

    global styles for headings

--- src/design/@generated/themes/dark.ts
+++ src/design/@generated/themes/dark.ts
@@ -4,6 +4,12 @@ import { createGlobalStyle } from 'styled-components';
 export default createGlobalStyle`
     :root {
         --font-display: -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Segoe UI", "Fira Sans", Roboto, Ubuntu, "Droid Sans", "Arial", sans-serif;
+        --gap-xs: 0.25rem;
+        --gap-s: 0.5rem;
+        --gap-sm: 0.75rem;
+        --gap-m: 1rem;
+        --gap-l: 2rem;
+        --gap-xl: 4rem;
         --background-color: #121212;
         --text-color-primary: #fff;
         --card-border-color: rgba(255, 255, 255, .1);
